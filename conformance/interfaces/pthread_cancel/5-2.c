e93de7d56fcc16c728bcd077b958b22d275e972d
Jukka Jylänki <jujjyl@gmail.com>
2015-02-03 18:32:48 +0200

    Skip test pthread_cancel/5-2.c since Emscripten does not support signals.

--- conformance/interfaces/pthread_cancel/5-2.c
+++ conformance/interfaces/pthread_cancel/5-2.c
@@ -239,6 +239,11 @@ int main ( int argc, char * argv[] )
 
 	struct sigaction sa;
 
+#ifdef __EMSCRIPTEN__
+       printf("Test SKIPPED: signals are not supported in Emscripten.\n");
+       exit(0);
+#endif
+
 	/* Initialize output routine */
 	output_init();
 
