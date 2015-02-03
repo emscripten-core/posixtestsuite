bbc7d51224e72764e52ac8f30f685b97d11c89ad
Jukka Jylänki <jujjyl@gmail.com>
2015-02-03 16:52:46 +0200

    Skip tests requiring pthread_mutexattr_setprotocol.

--- conformance/interfaces/pthread_mutexattr_setprotocol/1-1.c
+++ conformance/interfaces/pthread_mutexattr_setprotocol/1-1.c
@@ -19,6 +19,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <sched.h>
+#include <stdlib.h>
 #include "posixtest.h"
 
 int main()
@@ -26,6 +27,11 @@ int main()
 	
 	pthread_mutexattr_t mta;
 	int protocol, protcls[3],i;
+
+#ifdef __EMSCRIPTEN__
+	printf("Test SKIPPED: Emscripten does not support pthread_mutexattr_setprotocol to a nonzero value.\n");
+	exit(0);
+#endif
 	
 	/* Initialize a mutex attributes object */
 	if(pthread_mutexattr_init(&mta) != 0)
