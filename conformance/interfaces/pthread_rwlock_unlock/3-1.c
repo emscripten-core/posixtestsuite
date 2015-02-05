f402d7972254eef15fb6379022b2661286c5967f
Jukka Jylänki <jujjyl@gmail.com>
2015-02-06 01:30:07 +0200

    Skip pthread_rwlock_unlock/3-1.c since Emscripten does not support thread prioritization.

--- conformance/interfaces/pthread_rwlock_unlock/3-1.c
+++ conformance/interfaces/pthread_rwlock_unlock/3-1.c
@@ -197,6 +197,11 @@ int main()
 	pthread_t writer1, reader, writer2;
 	int priority;
 
+#ifdef __EMSCRIPTEN__
+	printf("Test SKIPPED: musl/Emscripten does not support thread priorities, so cannot test rwlocking in priority order.\n");
+	exit(0);
+#endif
+
 	/* main thread needs to have the highest priority*/
 	priority = sched_get_priority_min(TRD_POLICY) + 3;
 	set_priority(pthread_self(), TRD_POLICY, priority);
