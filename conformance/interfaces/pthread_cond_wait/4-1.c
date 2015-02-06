d17f717ad5c49b0569918d50eeabf4fcddde4437
Jukka Jylänki <jujjyl@gmail.com>
2015-02-06 18:05:26 +0200

    Add missing test PASS print in pthread_cond_wait/4-1.c

--- conformance/interfaces/pthread_cond_wait/4-1.c
+++ conformance/interfaces/pthread_cond_wait/4-1.c
@@ -307,6 +307,7 @@ int main (int argc, char * argv[])
 	#ifdef WITH_SYNCHRO
 	output("  %d signals were sent meanwhile.\n", count_sig);
 	#endif 
-	#endif	
+	#endif
+	printf("Test PASS\n");
 	PASSED;
 }
