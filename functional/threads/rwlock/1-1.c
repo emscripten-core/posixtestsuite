6fdd937d43902859ee7f50c57861ba2a5e005334
HongJunxin <875433256@qq.com>
2019-09-07 18:21:18 +0800

    move rwlock writer-prefered test to functional module

--- functional/threads/rwlock/1-1.c
+++ functional/threads/rwlock/1-1.c
@@ -1,5 +1,4 @@
 /*   
- * Copyright (c) 2019, HongJunxin. All rights reserved.
  * Created by:  HongJunxin
  * This file is licensed under the GPL license.  For the full content
  * of this license, see the COPYING file at the top level of this 
